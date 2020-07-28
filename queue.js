8dd49d141e6fc93408bbf361d0b855b243c2ccae
lyucean <lyucean@gmail.com>
2020-07-28 12:59:00 +0300

    ref queue.js

--- queue.js
+++ queue.js
@@ -2,37 +2,41 @@
 // для последовательного вызова функций.
 
 function Queue() {
-  this.q = [];
+  this._q = []; // массив с функциями
+
   // Добавляет вызов в очередь
-  this.add = function(foo, scope, attr){
-    q.push(Array.prototype.slice.call(arguments));
-  }
+  this.add = function(foo, scope, attr) {
+    this._q.push(Array.prototype.slice.call(arguments)); // копируем с окружением
+  };
+
   // Проигрывает очередь
   this.next = function() {
-    if (!this.q.length) return;
-    let opts = this.q.shift();
-    if (opts.length = 1) return opts[0]();
-    return opts[0].apply(opts[1], opts[2]||[]);
-  }
+
+    if (!this._q.length) { // Если больше нет
+      return;
+    }
+
+    let opts = this._q.shift(); // берём первый элемент
+
+    if (opts.length === 1) { // если функция без аргументов
+      return opts[0]();
+    }
+
+    return opts[0].apply(opts[1], opts[2] || []); // если с аргументами
+  };
 }
 
 // Создаем экземпляр очереди
 let q = new Queue();
+let count = 0;
 // Функция, имитирующая анимацию с задержкой в 1 сек
-let animaton = function(){
-  console.log('очень много анимации');
-  setTimeout(function(){
+let animation = function() {
+  console.log('очень много анимации ' + count++);
+  setTimeout(function() {
     q.next(); // Крутим очередь дальше
   }, 1000);
-}
+};
 
 animation();
-q.add(animation);
-q.add(animation);
-q.add(animation);
-q.add(animation);
-q.add(animation);
-q.add(animation);
-q.add(animation);
 q.add(animation);
 q.add(animation);
